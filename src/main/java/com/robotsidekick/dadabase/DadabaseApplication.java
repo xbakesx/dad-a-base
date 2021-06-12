547c3ac4658a3b6c48d20f78c8dd38a448119267
Alex Baker <alex.baker@jamf.com>
2021-06-12 11:10:17 -0500

    Initial spring boot commit from spring initialzr

---
+++ src/main/java/com/robotsidekick/dadabase/DadabaseApplication.java
@@ -0,0 +1,13 @@
+package com.robotsidekick.dadabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robotsidekick/dadabase/DadabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotsidekick.dadabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
